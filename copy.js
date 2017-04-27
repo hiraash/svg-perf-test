7258339c9c81078c20f87fed3a94f6e912d03d27
Thanish <mnmtanish@gmail.com>
2017-04-27 14:27:35 +0530

    Update index.html

--- copy.js
+++ copy.js
@@ -1,6 +1,7 @@
 module.exports = {
     copyScripts: {
         src: [
+            '{{ROOT}}/src/stats.min.js',
             '{{ROOT}}/node_modules/snapsvg/dist/snap.svg.js',
         ],
         dest: '{{WWW}}/build/'
